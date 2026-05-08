00561aa14c8bef4c009c68b5232a73d2bad16494
vinoth kumar <vinothkumar@vinoths-MacBook-Air.local>
2026-05-08 13:32:56 -0700

    update

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from EKS + GitHub Actions"
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy",
+        "environment": os.getenv("ENVIRONMENT", "dev")
+    }
+
+@app.route("/version")
+def version():
+    return {
+        "version": "1.0.0"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
